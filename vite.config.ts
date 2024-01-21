018284c0cdd414349a3e4d5578fc6e57d4322349
Lina Chen <sakura0477@gmail.com>
2024-01-21 16:42:17 +0800

    add router, gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑
+  base: process.env.NODE_ENV === 'production' ? '/freyja-react/' : '/',
   plugins: [react()],
 })
