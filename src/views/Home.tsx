fb4043ba088d8a24c35321721f0ba5dbc3887196
Lina Chen <sakura0477@gmail.com>
2024-01-21 17:00:43 +0800

    add bootstrap and custom variable

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
     return (<>
         <h1>首頁</h1>
+        <button className="btn btn-primary">Primary</button>{' '}
+        <button className="btn btn-secondary">Secondary</button>{' '}
     </>)
 };
 
