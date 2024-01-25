1b678336f55a296ba025fc4f094629d2453630b1
Lina Chen <sakura0477@gmail.com>
2024-01-25 09:15:38 +0800

    add sweetalert, material icon

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { HashRouter } from 'react-router-dom';
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'material-icons/iconfont/material-icons.css';
 import './assets/scss/all.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
